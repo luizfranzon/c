fe0b0b19caa2570b2846783fb3b6e1c5eee4db27
Luiz <luizfranzon05@hotmail.com>
2022-09-15 12:04:29 -0300

    syntax

---
+++ syntax/dataTypes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    int x;             //Tipo Inteiro (-32767 a 32767);
+    float a;           //Números reais
+    double b;          //Números reais porém com uma precisão melhor
+    char nome[20];     // Uma string. Deve ser em aspas duplas " "
+    int e;             //Valor lógico. 0 é falso e acima disso verdadeiro
+
+    return 0;
+}
